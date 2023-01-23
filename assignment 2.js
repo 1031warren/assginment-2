88d26a2106354c0d06f93a02023106b66236c1c4
Adam Warren <113055287+1031warren@users.noreply.github.com>
2023-01-22 19:38:11 -0700

    Add files via upload

---
+++ assignment 2.js
@@ -0,0 +1,13 @@
+
+
+function displayText(){
+     document.getElementById("myText").innerHTML = "Test number 1.";
+}
+
+function displayText2(htmlElement){
+    document.getElementById(htmlElement).innerHTML = "Here is another test.";
+}
+
+function displayText3(htmlElement){
+    document.getElementById(htmlElement).innerHTML = "Adam Warren, email: akw4382@outlook.com.";
+}
